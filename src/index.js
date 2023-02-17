9ee18e6b28704f6e1ea7a6c48ab081990a127bd2
YURII SILIN <try2scumme@gmail.com>
2023-02-17 07:07:53 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
 
 import App from './App';
@@ -9,11 +8,9 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <HashRouter>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </HashRouter>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </BrowserRouter>
 
 
